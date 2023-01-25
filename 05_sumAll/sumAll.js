3112515d6d5cbd4e0d004a7c6cea4b2ad26edbab
Jeff Makings <jeffmakings@gmail.com>
2023-01-24 19:09:38 -0800

    Update to add errors for negatives and non-number entries

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function(int1, int2) {
-    summation = 0
+const sumAll = function(input1, input2) {
+    if (typeof(input1) != "number" || typeof(input2) != "number") {
+        return "ERROR"
+    }
+    var summation = 0
+    var int1 = Math.min(input1, input2)
+    var int2 = Math.max(input1, input2)
+    
+    if (int1 < 0 ) {
+        return "ERROR"
+    }
+
     for (var i = int1; i <= int2; i++) {
         summation = summation + i
     }
