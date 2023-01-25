07cc7cd54cc394ae63f049d32158e3f3397d7acf
Jeff Makings <jeffmakings@gmail.com>
2023-01-24 19:03:01 -0800

    Initial sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    summation = 0
+    for (var i = int1; i <= int2; i++) {
+        summation = summation + i
+    }
+    return summation
 };
 
 // Do not edit below this line
