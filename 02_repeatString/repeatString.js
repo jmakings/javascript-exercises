3747869e978423a9b41376644027e54c77dba223
Jeff Makings <jeffmakings@gmail.com>
2023-01-24 16:45:05 -0800

    Fixed errors, tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0)
+        return 'ERROR'
+        
+    ans_str = ''
+    for (i = 0; i < num; i++) {
+        ans_str = ans_str + string
+    }
+    return ans_str
 };
 
 // Do not edit below this line
