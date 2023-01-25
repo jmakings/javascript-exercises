dd81f530891a1eff703232523dbe6ad2594409ff
Jeff Makings <jeffmakings@gmail.com>
2023-01-24 17:23:23 -0800

    Added conditional to loop, tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
 
+    for (const arg of toRemove) {
+        if (array.includes(arg) ) {
+            var index = array.indexOf(arg)
+            array.splice(index, 1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
